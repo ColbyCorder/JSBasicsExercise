2a48bda34c22af3c8937444d9682d33aa8319a76
“ColbyCorder” <colbycorder@gmail.com>
2022-02-28 18:32:46 -0500

    Created JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,55 @@
+/* 1 */
+console.log(1 + 2); // 3
+
+/* 2 */
+// Numbers, Null, Strings, Booleans, Undefined
+
+/* 3 */
+// + , - , * , / , ** , % 
+
+/* 4 */
+console.log(6 % 5); //1
+console.log(5 % 4); //1
+
+/* 5 */
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+/* 6 */
+console.log("Hawk" + "Eye");
+
+/* 7 */
+console.log("Colby".length + "Corder".length) //11
+
+/* 8 */
+console.log("Avengers Endgame" [15]); //e
+
+/*BONUS */
+/* 1 */
+console.log("\\");
+
+/* 2 */
+console.log(5 ** 4 - 9);
+
+/* 3 */
+console.log("I".length - "Am".length + "Iron".length % "Man".length); //0
+
+console.log("I".length % "Am".length - "Iron".length + "Man".length); //0
+
+console.log("I".length % "Am".length - "Iron".length % "Man".length); //0
+
+/* 4 */
+console.log("CAP" [1]);
+console.log("VISION" [0]);
+console.log("OKOYE" [4]);
+console.log("NICK FURY" [0]);
+console.log("GROOT" [0]);
+console.log("DOCTOR STRANGE" [13]);
+console.log("THOR" [3]);
+console.log("STAR-LORD" [0]);
+
+/* 5 */
+var m=25;
+var c=4;
+var u=c+m;
+
+console.log("The variable of u is:" + u)
